d8f3d18b5a027d6790176e1664c4c0cb8c77ec03
sonu.kumar <amitgrochdennis@yahoo.com>
2017-11-14 11:10:53 +0530

    remove componet

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,3 @@
-import { UserModule } from './user/user.module';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -6,21 +5,15 @@ import { AppComponent } from './app.component';
 import 'hammerjs';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HistoryIncommingCallsComponent } from './history-incoming/history-incomming-calls/history-incomming-calls.component';
-import { HistoryIncommingReceivedComponent } from './history-incoming/history-incomming-received/history-incomming-received.component';
-import { HistoryIncommingRejectComponent } from './history-incoming/history-incomming-reject/history-incomming-reject.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HistoryIncommingCallsComponent,
-    HistoryIncommingReceivedComponent,
-    HistoryIncommingRejectComponent
+    AppComponent  
   ],
   imports: [
     BrowserModule,
-    UserModule,
+  
     BrowserAnimationsModule
   ],
   providers: [],
